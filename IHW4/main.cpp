12764e92554f0b340084a3c2183a082b5e25f21e
tttuuu13 <tim.ib.222005@gmail.com>
2024-12-17 21:35:01 +0100

    m

--- IHW4/main.cpp
+++ IHW4/main.cpp
@@ -1,12 +1,32 @@
 #include <iostream>
-#include <map>
-#include <string>
-#include <thread>
+#include <pthread.h>
 #include <vector>
+#include <unordered_map>
+#include <string>
 #include <mutex>
-#include <random>
+#include <fstream>
 
-std::map<char, std::string> cipher_table = {
+std::mutex mtx;
+std::condition_variable cv;
+
+// Кастомная структура
+struct Fragment {
+  int index;
+  std::string encoded;
+};
+
+// Список фрагментов
+std::vector<Fragment> fragments;
+
+// Компаратор для фрагментов (для сортировки по индексу)
+struct FragmentComparator {
+  bool operator()(const Fragment& a, const Fragment& b) {
+    return a.index < b.index;
+  }
+};
+
+// Словарь с кодами для букв
+std::unordered_map<char, std::string> code_table = {
     {'A', "01"}, {'B', "02"}, {'C', "03"}, {'D', "04"}, {'E', "05"},
     {'F', "06"}, {'G', "07"}, {'H', "08"}, {'I', "09"}, {'J', "10"},
     {'K', "11"}, {'L', "12"}, {'M', "13"}, {'N', "14"}, {'O', "15"},
@@ -15,39 +35,109 @@ std::map<char, std::string> cipher_table = {
     {'Z', "26"}, {' ', "00"}
 };
 
-std::mutex mtx;
-std::vector<std::pair<int, std::string>> encodedFragments;
-
-void encodeFragment(const std::string& fragment, int index) {
+// Функция для кодирования фрагмента
+std::string encode_fragment(const std::string& fragment) {
   std::string encoded;
-
-  for (char ch : fragment) {
-    char upperCh = std::toupper(ch);
-    if (cipher_table.find(upperCh) != cipher_table.end()) {
-      encoded += cipher_table[upperCh] + " ";
+  for (char c : fragment) {
+    c = std::toupper(c);
+    if (code_table.count(c)) {
+      encoded += code_table[c] + " ";
     }
   }
-
-  {
-    std::lock_guard<std::mutex> lock(mtx);
-    encodedFragments.emplace_back(index, encoded);
-    std::cout << "Fragment " << index << " encoded: " << encoded << std::endl;
-  }
+  return encoded;
 }
 
-int main() {
-  std::cout << "Введите текст: ";
-  std::string text;
-  std::getline(std::cin, text);
+// Функция для потоков
+void* func(void* arg) {
+  auto* data = reinterpret_cast<std::pair<int, std::string>*>(arg);
+  int index = data->first;
+  std::string text = data->second;
 
-  int NUM_THREADS = 4;
-  std::vector<std::thread> threads;
-  std::vector<std::string> fragments;
+  // Кодируем фрагмент
+  std::string encoded = encode_fragment(text);
 
-  int fragmentSize = text.size() / NUM_THREADS;
-  int i = 0;
-  while (i < text.size()) {
-    int subStrSize = std::min(fragmentSize, text.size() - i);
+  // Добавляем результат в портфель
+  {
+    std::lock_guard<std::mutex> lock(mtx);
+    fragments.push_back({index, encoded});
   }
+
+  // Сообщаем о том что фрагмент был добавлен
+  cv.notify_one();
+  delete data;
+  return nullptr;
+}
+
+std::string generate_random_text(int min_length, int max_length) {
+  const std::string charset = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+  const int charset_size = charset.size();
+
+  int length = min_length + rand() % (max_length - min_length + 1);
+
+  std::string result;
+  result.reserve(length);
+  for (int i = 0; i < length; ++i) {
+    result += charset[rand() % charset_size];
+  }
+
+  return result;
+}
+
+int main(int argc, char* argv[]) {
+  if (argc < 3) {
+    std::cerr << "Недостаточно аргументов. Пример использования: ./program <text_or_random> <num_threads>" << std::endl;
+    return 1;
+  }
+
+  srand(static_cast<unsigned>(time(nullptr)));
+  std::string input_text;
+  int num_threads = std::stoi(argv[2]);
+
+  // Проверка, если текст был передан через аргумент
+  if (std::string(argv[1]) == "random") {
+    input_text = generate_random_text(5, 100);
+    std::cout << "Сгенерированный текст: " << input_text << std::endl;
+  } else {
+    input_text = argv[1];
+  }
+
+  // Делим текст на фрагменты
+  int fragment_size = input_text.size() / num_threads;
+  std::vector<std::pair<int, std::string>> tasks;
+
+  for (int i = 0; i < num_threads; ++i) {
+    int start = i * fragment_size;
+    int end = (i == num_threads - 1) ? input_text.size() : start + fragment_size;
+    tasks.emplace_back(i, input_text.substr(start, end - start));
+  }
+
+  // Создаем потоки
+  std::vector<pthread_t> threads(num_threads);
+  for (int i = 0; i < num_threads; ++i) {
+    auto* task = new std::pair<int, std::string>(tasks[i]); // Create copy of the task
+    pthread_create(&threads[i], nullptr, func, task);
+  }
+
+  // Ждем окончания выполнения потоков
+  for (pthread_t& thread : threads) {
+    pthread_join(thread, nullptr);
+  }
+
+  for (auto& fragment : fragments) {
+    std::cout << "Закодирован фрагмент " << fragment.index << ": " << fragment.encoded << std::endl;
+  }
+
+  // Сортируем закодированные фрагемнты по индексу
+  {
+    std::lock_guard<std::mutex> lock(mtx);
+    std::sort(fragments.begin(), fragments.end(), FragmentComparator());
+  }
+
+  std::cout << "\nИтоговый закодированный текст: ";
+  for (const auto& fragment : fragments) {
+    std::cout << fragment.encoded;
+  }
+  std::cout << std::endl;
+
   return 0;
 }
